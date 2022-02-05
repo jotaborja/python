b905d5f86789a223123d638dcb48ffce4019a334
jotaborja <shiwaah@gmail.com>
2022-02-04 22:24:49 -0500

    añadiendo 4 ejercicio del curso resuelto

---
+++ ejercicios/ex_04.py
@@ -0,0 +1,16 @@
+"""
+    Agarra el siguiente codigo que almacena una string
+
+    str = 'XSPAM-Confidence: 0.8475 '
+
+    Usa find y string slicing para extraer la porcion de la string despues del colon character y despues una la funcion float para convertir la cadena extraida en una numero punto flotante
+"""
+
+str = 'X-DSPAM-Confidence: 0.8475'
+
+encuentra = str.find(":")
+print(encuentra)
+num = str[encuentra+2:]
+print(num)
+numfloat = float(num)
+print(type(numfloat))
