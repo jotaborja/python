1d7fcf249d87c19bd086cb426ac5f58e60e480b9
jotaborja <shiwaah@gmail.com>
2022-02-13 13:48:29 -0500

    ejercicio_01 loops

---
+++ practica/loops/ejercicio_01.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 1
+Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+
+"""
+
+word = input("escrbie algo : ")
+
+for i in range(10):
+    print(word)
+    
